4e5f6b3f0dfdca597d53ba571c861f609ad45ae0
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-02 15:44:33 -0500

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/ClipboardHelper.swift

--- Shared/ClipboardHelper.swift
+++ Shared/ClipboardHelper.swift
@@ -7,28 +7,28 @@
 
 import Foundation
 #if canImport(UIKit)
-import UIKit
+    import UIKit
 #endif
 #if canImport(AppKit)
-import AppKit
+    import AppKit
 #endif
 
 enum ClipboardHelper {
     static func getText() -> String? {
-#if canImport(UIKit)
-    return UIPasteboard.general.string
-#else
-    return NSPasteboard.general
-#endif
+        #if canImport(UIKit)
+            return UIPasteboard.general.string
+        #else
+            return NSPasteboard.general
+        #endif
     }
-    
+
     static func set(text: String) {
-#if canImport(UIKit)
-    UIPasteboard.general.string = text
-#else
-    let pasteBoard = NSPasteboard.general
-    pasteBoard.clearContents()
-    pasteBoard.writeObjects([(text) as NSString])
-#endif
+        #if canImport(UIKit)
+            UIPasteboard.general.string = text
+        #else
+            let pasteBoard = NSPasteboard.general
+            pasteBoard.clearContents()
+            pasteBoard.writeObjects([text as NSString])
+        #endif
     }
 }
