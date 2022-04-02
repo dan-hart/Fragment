2bb44c5ba5b6f81225bbede6b5261685212b3e2b
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-02 15:32:28 -0500

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Views/Add/AddGistView.swift

--- Shared/Views/Add/AddGistView.swift
+++ Shared/Views/Add/AddGistView.swift
@@ -24,7 +24,7 @@ struct AddGistView: View {
 
     var didAdd: (Gist) -> Void
     
-    func getSaveButton() -> some Button<View> {
+    func getSaveButton() -> some Button<AnyView> {
         let ext = ".\(language.rawValue)"
         if !filename.ends(with: ext) {
             filename.append(ext)
