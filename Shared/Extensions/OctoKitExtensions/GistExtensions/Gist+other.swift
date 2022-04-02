0368a7486b5eb83c508fc897f20f737970a40a63
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-02 14:48:30 -0500

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Extensions/OctoKitExtensions/GistExtensions/Gist+other.swift

--- Shared/Extensions/OctoKitExtensions/GistExtensions/Gist+other.swift
+++ Shared/Extensions/OctoKitExtensions/GistExtensions/Gist+other.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import OctoKit
+
+extension Gist {
+    var id: String? {
+        parent.id
+    }
+
+    var cacheKey: String {
+        parent.files.first?.key ?? (id ?? UUID().uuidString)
+    }
+
+    var ext: String {
+        let filename: NSString = (parent.files.first?.key ?? "") as NSString
+        return filename.pathExtension
+    }
+
+    // swiftlint:enable identifier_name
+
+    init(parent: Gist) {
+        self.parent = parent
+    }
+
+    // MARK: - Functions
+
+    func meetsSearchCriteria(text: String) -> Bool {
+        let descriptionContainsText = parent.description?.lowercased().contains(text.lowercased()) ?? false
+        let filenameContainsText = parent.files.first?.value.filename?.lowercased().contains(text.lowercased()) ?? false
+
+        if descriptionContainsText || filenameContainsText {
+            return true
+        } else {
+            return false
+        }
+    }
+}
