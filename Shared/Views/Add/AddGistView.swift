cc79d097c2bf0ff596d9c6c0867aaf74686f7ad1
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-02 15:43:13 -0500

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Views/Add/AddGistView.swift

--- Shared/Views/Add/AddGistView.swift
+++ Shared/Views/Add/AddGistView.swift
@@ -78,9 +78,15 @@ struct AddGistView: View {
                 CodeEditor(source: $content, language: CodeEditor.Language(rawValue: language.rawValue))
                     .font(.system(.caption, design: .monospaced))
                     .frame(minHeight: 100)
-                    .onAppear {
-                        content = ClipboardHelper.getText()
-                    }
+                Button {
+                    content = ""
+                } label: {
+                    <#code#>
+                }
+
+            }
+            .onAppear {
+                content = ClipboardHelper.getText()
             }
 
             // getSaveButton()
