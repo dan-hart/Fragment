b900098815613f9a8359a2d90049e5971a04cd8f
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 11:30:23 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Views/SnippetsListView.swift

--- Shared/Views/SnippetsListView.swift
+++ Shared/Views/SnippetsListView.swift
@@ -108,7 +108,7 @@ struct SnippetsListView: View {
             }
         }
         .onAppear {
-            gists = cacheHandler.gistsCache.value<V: [Gist]>(forKey: "\(CacheHandler.Key.gists.rawValue)")
+            gists = cacheHandler.gistsCache.value(forKey: "\(CacheHandler.Key.gists.rawValue)")
         }
         .refreshable {
             Task {
