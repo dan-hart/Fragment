62824cd44bfd2c95d847e34188c7a73fec73285d
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 13:05:37 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Views/MainView.swift

--- Shared/Views/MainView.swift
+++ Shared/Views/MainView.swift
@@ -19,7 +19,6 @@ struct MainView: View {
             if isLoading {
                 #if os(macOS)
                     EmptyView()
-                        .frame(minWidth: 100)
                 #endif
                 Text("Loading")
                     .font(.system(.body, design: .monospaced))
