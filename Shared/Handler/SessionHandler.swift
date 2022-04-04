1c1dff3cc04e1103a749992361a74da9202d23b3
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 16:03:02 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Handler/SessionHandler.swift

--- Shared/Handler/SessionHandler.swift
+++ Shared/Handler/SessionHandler.swift
@@ -171,7 +171,7 @@ class SessionHandler: ObservableObject {
         if !isAuthenticated { throw FragmentError.notAuthenticated }
     }
     
-    func call<T>(sessionFunction: @autoclosure () async throws -> T) async -> T {
+    func call<T>(sessionFunction: @autoclosure () -> Void) {
         do {
             let value = try await sessionFunction()
         }
