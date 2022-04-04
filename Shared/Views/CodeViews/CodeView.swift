321e72047585973000add7b18c55c54d100fc5bb
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 14:43:46 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Views/CodeViews/CodeView.swift

--- Shared/Views/CodeViews/CodeView.swift
+++ Shared/Views/CodeViews/CodeView.swift
@@ -38,8 +38,8 @@ struct CodeView: View {
         }
         .content.offset(x: 0, y: 0)
         .onChange(of: gist, perform: { newValue in
-            loadedSourceCode = newValue?.text
-            sourceCode = newValue?.text
+            loadedSourceCode = newValue?.text ?? ""
+            sourceCode = newValue?.text ?? ""
         })
         .toolbar {
             ToolbarItem {
