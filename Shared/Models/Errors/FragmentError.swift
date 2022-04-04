6a31c725e58d59679db137f63f7f65b27efd658c
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 17:35:07 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Models/Errors/FragmentError.swift

--- Shared/Models/Errors/FragmentError.swift
+++ Shared/Models/Errors/FragmentError.swift
@@ -11,9 +11,9 @@ enum FragmentError: String, Error {
     case nilToken = "Empty Token"
     case nilConfiguratioin = "Invalid Configuration"
     case invalidToken = "Provided Token is invalid"
-    case notAuthenticated = "Not authenticated"
+    case notAuthenticated = "Not Authenticated"
 
     // MARK: - Data
 
-    case couldNotFetchData = "Could not fetch data"
+    case couldNotFetchData = "Could not get data. Check your network connection."
 }
