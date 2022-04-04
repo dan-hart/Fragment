b1ce452abb3939297cf5e80a672bfc4286f5f332
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 15:29:45 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Handlers/SessionHandler.swift

--- Shared/Handlers/SessionHandler.swift
+++ Shared/Handlers/SessionHandler.swift
@@ -14,4 +14,75 @@ class SessionHandler: ObservableObject {
     static var keychainKeyIdentifier = "FRAGMENT_GITHUB_API_TOKEN"
 
     @Published var isAuthenticated = false
+    
+    var configuration: TokenConfiguration?
+
+    init() {}
+
+    // MARK: - Methods
+
+    func clear() {
+        delete(key: TokenHandler.keyName)
+        isAuthenticated = false
+    }
+
+    func taskCheckingAuthenticationStatus() {
+        Task {
+            await checkAuthenticationStatus()
+        }
+    }
+
+    /// returns true if authentication succeeded
+    func checkAuthenticationStatus() async -> Bool {
+        let optionalToken = token
+
+        do {
+            if let configuration = try await authenticate(using: optionalToken) {
+                self.configuration = configuration
+                await MainActor.run {
+                    isAuthenticated = true
+                }
+                return true
+            } else {
+                return false
+            }
+        } catch {
+            await MainActor.run {
+                isAuthenticated = false
+            }
+            print(error.localizedDescription)
+            return false
+        }
+    }
+
+    func save(token: String) {
+        keychain[TokenHandler.keyName] = token
+    }
+
+    func delete(key: String = TokenHandler.keyName) {
+        keychain[key] = nil
+        isAuthenticated = false
+    }
+
+    // MARK: - Authentication
+
+    func authenticate(using token: String?) async throws -> TokenConfiguration? {
+        guard let token = token else {
+            return nil
+        }
+
+        let configuration = TokenConfiguration(token)
+        let response = await withCheckedContinuation { continuation in
+            Octokit(configuration).me { response in
+                continuation.resume(returning: response)
+            }
+        }
+
+        switch response {
+        case .success:
+            return configuration
+        case let .failure(error):
+            throw error
+        }
+    }
 }
