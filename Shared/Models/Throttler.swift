c0ba9df872a43d52fcb5f89e61cbbd8bd629eae7
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 15:04:43 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Models/Throttler.swift

--- Shared/Models/Throttler.swift
+++ Shared/Models/Throttler.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+public class Throttler {
+    
+    private let queue: DispatchQueue = DispatchQueue.global(qos: .background)
+    
+    private var job: DispatchWorkItem = DispatchWorkItem(block: {})
+    private var previousRun: Date = Date.distantPast
+    private var maxInterval: TimeInterval
+    
+    public init(maxInterval: TimeInterval) {
+        self.maxInterval = maxInterval
+    }
+    
+    public func throttle(block: @escaping () -> ()) {
+        job.cancel()
+        job = DispatchWorkItem(){ [weak self] in
+            self?.previousRun = Date()
+            block()
+        }
+        let delay = Date().timeIntervalSince(previousRun) > maxInterval ? 0 : maxInterval
+        queue.asyncAfter(deadline: .now() + Double(delay), execute: job)
+    }
+}
