25f476c8bf347111312cc9615ff79f21ddabefb1
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 11:55:20 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Views/SettingsView.swift

--- Shared/Views/SettingsView.swift
+++ Shared/Views/SettingsView.swift
@@ -46,6 +46,8 @@ struct SettingsView: View {
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsView()
+        Settings {
+            SettingsView()
+        }
     }
 }
