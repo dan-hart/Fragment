506fe2bd1b523aa3b309c37e34342d2a7eb23ba5
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 09:23:42 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Views/SnippetsListView.swift

--- Shared/Views/SnippetsListView.swift
+++ Shared/Views/SnippetsListView.swift
@@ -20,7 +20,7 @@ struct SnippetsListView: View {
     @State var searchText = ""
     @AppStorage("visibility") var visibility: Visibility = .public
 
-    @State var filteredGists: [Gist] {
+    var filteredGists: [Gist] {
         let withVisibility = gists.filter { gist in
             let gistVisibility = Visibility(isPublic: gist.publicGist)
             return gistVisibility == visibility
