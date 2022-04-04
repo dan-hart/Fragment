a099be1022c350031d6510e565b48616f22340e9
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 14:27:18 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/FragmentApp.swift

--- Shared/FragmentApp.swift
+++ Shared/FragmentApp.swift
@@ -20,11 +20,11 @@ struct FragmentApp: App {
             MainView(isLoading: $isLoading)
                 .environmentObject(tokenHandler)
                 .environmentObject(octoHandler)
-                .task {
-                    isLoading = true
-                    _ = await tokenHandler.checkAuthenticationStatus()
-                    isLoading = false
-                }
+//                .task {
+//                    isLoading = true
+//                    _ = await tokenHandler.checkAuthenticationStatus()
+//                    isLoading = false
+//                }
         }
 
         #if os(macOS)
