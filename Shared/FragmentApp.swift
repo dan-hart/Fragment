b15216759c8d33ce312e3b469b41004d7a6e701c
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 16:36:45 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/FragmentApp.swift

--- Shared/FragmentApp.swift
+++ Shared/FragmentApp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 @main
 struct FragmentApp: App {
-    @StateObject let sessionHandler = SessionHandler()
+    @StateObject var sessionHandler = SessionHandler()
 
     @State var isLoading = false
     @State var isSettingsLoading = false
