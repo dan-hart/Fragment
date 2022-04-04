34a16d08a728a7d2860b48079589fe1be5416217
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 14:47:38 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Views/Container/ListView.swift

--- Shared/Views/Container/ListView.swift
+++ Shared/Views/Container/ListView.swift
@@ -98,7 +98,7 @@ struct ListView: View {
                     Group {
                     GistRow(data: $octoHandler.gists[index])
                     }
-                    .background((selectedGist?.identifier == octoHandler.gists[index].identifier ? .blue : nil)
+                    .background((selectedGist == octoHandler.gists[index] ? .blue : nil)
                     .contentShape(Rectangle())
                         .onTapGesture {
                             selectedGist = octoHandler.gists[index]
