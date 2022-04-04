a091533e231024fd5c5136a64c1139d2d4dffad8
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 12:29:04 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/FragmentApp.swift

--- Shared/FragmentApp.swift
+++ Shared/FragmentApp.swift
@@ -37,5 +37,6 @@ struct FragmentApp: App {
                 .environmentObject(cacheHandler)
                 .frame(width: 400, height: 400)
         }
+        .redacted(reason: isLoading ? .placeholder : [])
     }
 }
