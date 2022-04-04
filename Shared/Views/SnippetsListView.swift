d90eecf198ecebfff9805943c80c8136e2c0ab0a
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 10:12:08 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Views/SnippetsListView.swift

--- Shared/Views/SnippetsListView.swift
+++ Shared/Views/SnippetsListView.swift
@@ -95,7 +95,7 @@ struct SnippetsListView: View {
                 AddGistView(filename: "", description: "", content: "") { newGist in
                     gists.insert(newGist, at: 0)
                 }
-                .frame(minWidth: 500)
+                .frame(minWidth: 800, minHeight: 800)
                 .padding()
             #endif
         })
