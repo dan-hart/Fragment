f747d3e0cf01d5b1a474874ba961822ecefdde9b
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 11:54:54 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Fragment.xcodeproj/project.pbxproj 	new file:   Shared/Views/SettingsView.swift

---
+++ Shared/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Fragment
+//
+//  Created by Dan Hart on 4/4/22.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
