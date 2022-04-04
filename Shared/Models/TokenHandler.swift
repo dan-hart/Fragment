a474981d0f2912dc9c3a4751e3270cea3c0837d7
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 09:08:06 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Models/TokenHandler.swift

--- Shared/Models/TokenHandler.swift
+++ Shared/Models/TokenHandler.swift
@@ -24,7 +24,6 @@ class TokenHandler: ObservableObject {
     }
     
     private var configuration: TokenConfiguration?
-    private var value: String?
 
     init() {
         let optionalToken = getToken()
