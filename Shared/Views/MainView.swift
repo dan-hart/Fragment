31f3bffccb54b91804cb2f6dd683bfa615b2fc97
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 16:55:49 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Views/MainView.swift

--- Shared/Views/MainView.swift
+++ Shared/Views/MainView.swift
@@ -27,6 +27,8 @@ struct MainView: View {
                         .padding()
                     Text("Enter Github personal access token on the lefthand sidebar")
                         .font(.system(.body, design: .monospaced))
+                        
+                        .navigationTitle("Welcome to Fragment")
                 #endif
             }
         }
