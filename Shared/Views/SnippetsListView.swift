2809bf6775ffb8f7fd3dcf2fd0433e4cbb9a6c22
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 09:28:04 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Views/SnippetsListView.swift

--- Shared/Views/SnippetsListView.swift
+++ Shared/Views/SnippetsListView.swift
@@ -25,7 +25,7 @@ struct SnippetsListView: View {
             let gistVisibility = Visibility(isPublic: gist.publicGist)
             return gistVisibility == visibility
         }.filter { gist in // Search
-            searchText.isEmpty ? true : ()
+            gist.meetsSearchCriteria(text: searchText)
         }
 
         if searchText.isEmpty {
