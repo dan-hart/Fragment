5e0c4435a03433040d4461aa8ed6bdfb84c6b45f
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 10:40:50 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Models/TokenHandler.swift

--- Shared/Models/TokenHandler.swift
+++ Shared/Models/TokenHandler.swift
@@ -60,7 +60,7 @@ class TokenHandler: ObservableObject {
 
     // MARK: - Authentication
 
-    func authenticate(using token: String?, then: @escaping (TokenConfiguration?) -> Void) async {
+    func authenticate(using token: String?, then: @escaping (TokenConfiguration?) -> Void) async -> TokenConfiguration {
         guard let token = token else {
             return then(nil)
         }
