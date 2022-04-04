d67780c46fb615b894789184f7b615629bf8e05a
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 18:01:23 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Views/SettingsView.swift

--- Shared/Views/SettingsView.swift
+++ Shared/Views/SettingsView.swift
@@ -64,6 +64,7 @@ struct SettingsView: View {
                         }
                     }
                 }
+                }
                 .task {
                     isLoading = true
                     
