b0b303cfd6f7c75f4eb0ab7832a9658a10336c52
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 09:16:49 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Views/SnippetsListView.swift

--- Shared/Views/SnippetsListView.swift
+++ Shared/Views/SnippetsListView.swift
@@ -58,7 +58,7 @@ struct SnippetsListView: View {
                     NavigationLink {
                         CodeView(gist: .constant(gist), isLoadingParent: $isLoading)
                         #if os(macOS)
-                            .frame(minWidth: CGFloat(9999), idealWidth: nil, maxWidth: nil, minHeight: nil, idealHeight: nil, maxHeight: nil, alignment: .center)
+                            .frame(minWidth: CGFloat(NSScreen.main?.frame.width), idealWidth: nil, maxWidth: nil, minHeight: nil, idealHeight: nil, maxHeight: nil, alignment: .center)
                         #endif
                     } label: {
                         GistRow(data: gist)
