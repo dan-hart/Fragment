1756800f9fd5c71118acaa3bbaaa97cf8d000ab9
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 18:14:50 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Constants.swift

--- Shared/Constants.swift
+++ Shared/Constants.swift
@@ -17,6 +17,7 @@ enum Constants {
     }
 
     enum Feature {
+        // False
         static var ifNoGistsEnableCreateButton = false
         static var ifNoGistsEnablePullButton = false
         
