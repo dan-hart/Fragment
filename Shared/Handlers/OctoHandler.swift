f31aeaafa0650b8122dc01f191a656ddd48b731d
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 13:21:45 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Handlers/OctoHandler.swift

--- Shared/Handlers/OctoHandler.swift
+++ Shared/Handlers/OctoHandler.swift
@@ -97,7 +97,7 @@ class OctoHandler: ObservableObject {
 
     func gists(using configuration: TokenConfiguration?) async -> [Gist]? {
         guard let configuration = configuration else {
-            throw 
+            throw FragmentError.nilConfiguratioin
         }
 
         Octokit(configuration).myGists { response in
