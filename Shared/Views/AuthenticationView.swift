d120cf3bacaa49d2cd20c81ab15c0b6a7282c44c
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 13:16:57 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Views/AuthenticationView.swift

--- Shared/Views/AuthenticationView.swift
+++ Shared/Views/AuthenticationView.swift
@@ -13,43 +13,43 @@ struct AuthenticationView: View {
     @State var token: String = ""
 
     var body: some View {
-            VStack(alignment: .center) {
-                Spacer()
-                Text("Github Personal Access Token")
+        VStack(alignment: .center) {
+            Spacer()
+            Text("Github Personal Access Token")
+                .font(.system(.body, design: .monospaced))
+            SecureField(text: $token,
+                        prompt: Text("uZnVflqpqr2U1M9x984h3985a48dn74n").font(.system(.body, design: .monospaced))) {
+                Text("Token")
                     .font(.system(.body, design: .monospaced))
-                SecureField(text: $token,
-                            prompt: Text("uZnVflqpqr2U1M9x984h3985a48dn74n").font(.system(.body, design: .monospaced))) {
-                    Text("Token")
+            }
+            .onSubmit {
+                go()
+            }
+
+            Button {
+                go()
+            } label: {
+                HStack {
+                    Image(systemSymbol: SFSymbol.lock)
+                    Text("Validate")
                         .font(.system(.body, design: .monospaced))
                 }
-                .onSubmit {
-                    go()
-                }
-
+            }
+            Spacer()
+            if let url = URL(string: Constants.URL.githubHowToPersonalAccessToken.rawValue) {
                 Button {
-                    go()
+                    WebLauncher.go(to: url)
                 } label: {
                     HStack {
-                        Image(systemSymbol: SFSymbol.lock)
-                        Text("Validate")
+                        Image(systemSymbol: SFSymbol.questionmarkCircle)
+                        Text("How To")
                             .font(.system(.body, design: .monospaced))
                     }
                 }
                 Spacer()
-                if let url = URL(string: Constants.URL.githubHowToPersonalAccessToken.rawValue) {
-                    Button {
-                        WebLauncher.go(to: url)
-                    } label: {
-                        HStack {
-                            Image(systemSymbol: SFSymbol.questionmarkCircle)
-                            Text("How To")
-                                .font(.system(.body, design: .monospaced))
-                        }
-                    }
-                    Spacer()
-                }
             }
-            .padding()
+        }
+        .padding()
 
         .navigationTitle("Authentication")
     }
