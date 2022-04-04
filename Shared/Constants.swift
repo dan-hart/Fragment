311c3f6a216940eaad0a86e2081cf78f8ef60d49
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 14:12:57 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Constants.swift

--- Shared/Constants.swift
+++ Shared/Constants.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+#if canImport(UIKit)
 
 // swiftlint:disable line_length
 enum Constants {
@@ -15,6 +16,19 @@ enum Constants {
 
     enum Feature {
         static var ifNoGistsEnablePullButton = true
+    }
+    
+    /// Is the current device running macOS or is it an iPad
+    public var isMacOrPad: Bool {
+        #if os(macOS)
+        return true
+        #else
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            return true
+        } else {
+            return false
+        }
+        #endif
     }
 }
 
