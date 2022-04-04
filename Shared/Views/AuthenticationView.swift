36cbe855f668410348690649177396e940366bee
Dan Hart <dan-hart@users.noreply.github.com>
2022-04-04 10:52:17 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Shared/Views/AuthenticationView.swift

--- Shared/Views/AuthenticationView.swift
+++ Shared/Views/AuthenticationView.swift
@@ -58,7 +58,7 @@ struct AuthenticationView: View {
         tokenHandler.save(token: token)
         Task {
         await MainActor.run {
-            await tokenHandler.checkAuthenticationStatus()
+            tokenHandler.checkAuthenticationStatus()
         }
         }
     }
