5dc6b519c361acc9d50a95677e50170cf89ed249
Dan Hart <dhart94@gmail.com>
2022-04-04 20:03:14 -0400

    (use "git restore --staged <file>..." to unstage) 	modified:   Fragment.xcodeproj/project.pbxproj 	modified:   Fragment.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved 	new file:   Shared/Views/Support/SupportView.swift

---
+++ Shared/Views/Support/SupportView.swift
@@ -0,0 +1,20 @@
+//
+//  SupportView.swift
+//  Fragment
+//
+//  Created by Dan Hart on 4/4/22.
+//
+
+import SwiftUI
+
+struct SupportView: View {
+    var body: some View {
+        Text("Support")
+    }
+}
+
+struct SupportView_Previews: PreviewProvider {
+    static var previews: some View {
+        SupportView()
+    }
+}
